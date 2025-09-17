9a38a76d5a77fa4eb3f741ab93c59690c56efd0c
Martinabaus <martina.baus.corti@gmail.com>
2025-09-17 02:11:49 +0200

    update

--- ForMonica/sketch.js
+++ ForMonica/sketch.js
@@ -243,7 +243,7 @@ function setup() {
   // Play button
   button = createButton('▸');
   button.style('font-size', '16px');
-  button.style('padding', '8px 16px');
+  button.style('padding', '7px 16px');
   button.style('background', 'transparent');
   button.style('color', '#26de06ff');
   button.style('border', '1px solid #fefe02ff');
